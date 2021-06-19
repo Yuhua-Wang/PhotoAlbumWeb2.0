19ee930ee4385821f329fecb234721c1bc1f518c
DawnRising <yuhuawang0601@alumni.ubc.ca>
2021-06-19 12:41:54 +0800

    stored initial photos in JSON

--- photo-album/src/Backend/Server.js
+++ photo-album/src/Backend/Server.js
@@ -2,25 +2,26 @@ let express = require('express');
 let app = express();
 const PORT = process.env.PORT || 5000
 
-let database = [
-        {
-            "title": "Clock Tower",
-            "URL": "https://news.ubc.ca/wp-content/uploads/2019/04/UBC-1024x576.jpg",
-            "description": "A photo of Ladner Clock Tower and IKB library"
-        },
-        {
-            "title": "Fountain",
-            "URL": "https://images.dailyhive.com/20180606115422/ubc1.jpg",
-            "description": "A photo of the fountain"
-        },
-        {
-            "title": "Campus Overview",
-            "URL": "https://www.ctvnews.ca/polopoly_fs/1.1444716.1561483853!/httpImage/image.jpg_gen/derivatives/landscape_1020/image.jpg",
-            "description": "A photo of the campus from above"
-        }
-    ];
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+let databaseJSON = JSON.stringify( [
+    {
+        "title": "Clock Tower",
+        "URL": "https://news.ubc.ca/wp-content/uploads/2019/04/UBC-1024x576.jpg",
+        "description": "A photo of Ladner Clock Tower and IKB library"
+    },
+    {
+        "title": "Fountain",
+        "URL": "https://images.dailyhive.com/20180606115422/ubc1.jpg",
+        "description": "A photo of the fountain"
+    },
+    {
+        "title": "Campus Overview",
+        "URL": "https://www.ctvnews.ca/polopoly_fs/1.1444716.1561483853!/httpImage/image.jpg_gen/derivatives/landscape_1020/image.jpg",
+        "description": "A photo of the campus from above"
+    }
+]);
+let database = [];
 
 // get all photos
 app.get('/photos', (req, res) => {
@@ -53,9 +54,9 @@ app.delete('/photos/:index', (req, res) => {
 })
 
 let server = app.listen(PORT, function () {
-    let host = server.address().address
-    let port = server.address().port
-
+    let host = server.address().address;
+    let port = server.address().port;
+    database = JSON.parse(databaseJSON);
     console.log("server started on port: %s", port)
 })
 
