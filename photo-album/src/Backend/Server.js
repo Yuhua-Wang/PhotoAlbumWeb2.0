038cacdab7364e4e74bf361924feedbb311ce45f
DawnRising <yuhuawang0601@alumni.ubc.ca>
2021-06-19 12:45:55 +0800

    minor correction on the JSON

--- photo-album/src/Backend/Server.js
+++ photo-album/src/Backend/Server.js
@@ -4,23 +4,23 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-let databaseJSON = JSON.stringify( [
-    {
-        "title": "Clock Tower",
-        "URL": "https://news.ubc.ca/wp-content/uploads/2019/04/UBC-1024x576.jpg",
-        "description": "A photo of Ladner Clock Tower and IKB library"
-    },
-    {
-        "title": "Fountain",
-        "URL": "https://images.dailyhive.com/20180606115422/ubc1.jpg",
-        "description": "A photo of the fountain"
-    },
-    {
-        "title": "Campus Overview",
-        "URL": "https://www.ctvnews.ca/polopoly_fs/1.1444716.1561483853!/httpImage/image.jpg_gen/derivatives/landscape_1020/image.jpg",
-        "description": "A photo of the campus from above"
-    }
-]);
+let databaseJSON = JSON.stringify({"database": [
+        {
+            "title": "Clock Tower",
+            "URL": "https://news.ubc.ca/wp-content/uploads/2019/04/UBC-1024x576.jpg",
+            "description": "A photo of Ladner Clock Tower and IKB library"
+        },
+        {
+            "title": "Fountain",
+            "URL": "https://images.dailyhive.com/20180606115422/ubc1.jpg",
+            "description": "A photo of the fountain"
+        },
+        {
+            "title": "Campus Overview",
+            "URL": "https://www.ctvnews.ca/polopoly_fs/1.1444716.1561483853!/httpImage/image.jpg_gen/derivatives/landscape_1020/image.jpg",
+            "description": "A photo of the campus from above"
+        }
+    ]});
 let database = [];
 
 // get all photos
@@ -56,7 +56,7 @@ app.delete('/photos/:index', (req, res) => {
 let server = app.listen(PORT, function () {
     let host = server.address().address;
     let port = server.address().port;
-    database = JSON.parse(databaseJSON);
+    database = JSON.parse(databaseJSON).database;
     console.log("server started on port: %s", port)
 })
 
