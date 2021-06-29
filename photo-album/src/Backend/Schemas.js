73b95a50e9d365772eec91c78f63cc1c96c26bfb
DawnRising <yuhuawang0601@alumni.ubc.ca>
2021-06-29 16:12:56 +0800

    Implemented MongoDB and relevant functionalities

--- photo-album/src/Backend/Schemas.js
+++ photo-album/src/Backend/Schemas.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
 const PhotoSchema = mongoose.Schema({
-    _id: mongoose.SchemaTypes.ObjectId,
-    title: String,
-    URL: String,
-    Description: String
-})
+        _id: mongoose.SchemaTypes.ObjectId,
+        title: String,
+        URL: String,
+        description: String
+    },
+    {
+        collection: 'Photos'
+    })
 
 module.exports = mongoose.model('Photo', PhotoSchema);
--- photo-album/src/Backend/Server.js
+++ photo-album/src/Backend/Server.js
@@ -5,60 +5,85 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 const mongoose = require('mongoose');
+mongoose.set('useFindAndModify', false);
 const Photo = require('./Schemas');
-const mongoURL = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.uxphx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const DB_NAME = 'PhotoAlbum';
+const mongoURL = 'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.uxphx.mongodb.net/' + DB_NAME +'?retryWrites=true&w=majority';
 
 let server = app.listen(PORT, function () {
     let host = server.address().address;
     let port = server.address().port;
-    try {
-        mongoose.connect(mongoURL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-    } catch (e) {
-        console.log(e);
-    }
-    console.log("server started on port: %s", port);
+    mongoose.connect(mongoURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+        .then(res=>console.log("server started on port: %s", port))
+        .catch(err=>{console.log(err)});
+
 })
 
 // get all photos
 app.get('/photos', (req, res) => {
-    Photo.find()
+    getAllPhotos(res);
+})
+
+// add a new photo
+app.post('/photos', (req, res) => {
+    const newPhoto = new Photo({
+        _id: new mongoose.Types.ObjectId(),
+        title: req.body.title,
+        URL: req.body.URL,
+        description: req.body.description
+    })
+    newPhoto.save().then(result=>{
+        getAllPhotos(res);
+    }).catch(err => {
+        res.status(500).json(err);
+    })
+})
+
+// edit a photo
+app.put('/photos/:id', (req, res) => {
+    Photo.findByIdAndUpdate(req.params.id, {
+        title: req.body.title,
+        URL: req.body.URL,
+        description: req.body.description
+    }).then(result=>{
+        getAllPhotos(res);
+    }).catch(err => {
+        res.status(500).json(err);
+    })
+})
+
+// delete all photos
+app.delete('/photos', (req, res) => {
+    Photo.deleteMany({}).then(result=>{
+        getAllPhotos(res);
+    }).catch(err=>{
+        res.status(500).json(err);
+    })
+})
+
+// delete a single photo
+app.delete('/photos/:id', (req, res) => {
+    Photo.findByIdAndRemove(req.params.id).then(result=>{
+        getAllPhotos(res);
+    }).catch(err=>{
+        res.status(500).json(err);
+    })
+})
+
+function getAllPhotos(res){
+     Photo.find()
         .then(photos => {
-            console.log(photos);
+            // console.log(photos);
             res.status(200).json(photos);
         })
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
         })
-})
-
-// add a new photo
-app.post('/photos', (req, res) => {
-    let newPhoto = req.body;
-    photos.push(newPhoto);
-    res.json(photos);
-})
-
-// edit a photo
-app.put('/photos/:index', (req, res) => {
-    photos[req.params.index] = req.body;
-    res.json(photos);
-})
-
-// delete all photos
-app.delete('/photos', (req, res) => {
-    photos = [];
-    res.json(photos);
-})
-
-// delete a single photo
-app.delete('/photos/:index', (req, res) => {
-    photos.splice(req.params.index, 1);
-    res.json(photos);
-})
+}
 
 
 
