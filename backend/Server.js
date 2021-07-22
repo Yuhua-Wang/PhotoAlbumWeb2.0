00b0a9c1e47299f34d600bdbb5c2a10676cbcfef
DawnRising <yuhuawang0601@alumni.ubc.ca>
2021-07-22 12:30:00 +0800

    re-structured

--- backend/Server.js
+++ backend/Server.js
@@ -22,7 +22,7 @@ let server = app.listen(PORT, function () {
         .catch(err=>{console.log(err)});
 })
 
-app.use(express.static(path.join(__dirname, 'photo-album/build')));
+// app.use(express.static(path.join(__dirname, 'photo-album/build')));
 
 // get all photos
 app.get('/photos', (req, res) => {
