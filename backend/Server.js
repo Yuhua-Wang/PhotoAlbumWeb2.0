4a4f1ebe9f7117c925b74dc1062b35b081c642ea
DawnRising <yuhuawang0601@alumni.ubc.ca>
2021-07-22 11:59:27 +0800

    re-structured

--- backend/Server.js
+++ backend/Server.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+const path = require('path');
 const mongoose = require('mongoose');
 mongoose.set('useFindAndModify', false);
 const Photo = require('./Schemas');
